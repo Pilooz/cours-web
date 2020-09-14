40623ff91df49413a04f0a8bbeeb009347a81e82
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-09-14 15:20:59 +0200

    session 1

--- workshop/index.php
+++ workshop/index.php
@@ -1,1 +1,54 @@
-<h1 style="margin:25%">Nothing more there... <br/>Vous êtes arrivé au bout de l'internet.</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
+    <style>
+        .content {
+            background-color: pink;
+        }
+        .relative {
+            background-color: lightgreen;
+        }
+
+        .rubriqueActive {
+            color:white;
+            background-color: gray;
+        }
+    </style>
+
+</head>
+<body>
+<div class="container">
+<?php
+    require('header.php');
+ ?>
+  <div class="row">
+<?php
+    if (isset($_GET['nav'])) {
+        $navig = $_GET['nav'];    
+    } else {
+        $navig = "index";
+    }
+       
+    switch ($navig) {
+        case 'index':
+            require('content.php');
+            break;
+        
+        case 'todo':
+            require('todo.php');
+            break;
+            
+            default:
+            require('content.php');
+            break;
+    }
+?>
+<?php 
+require('relative.php');
+?>
+<?php
+require('footer.php');
+?>
+</body>
+</html>
--- workshop/todo.php
+++ workshop/todo.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
-</head>
-<body>
-<div class="container">
-    <header>
-        <a href="#" class="logo">Workshop de Septembre 2020</a>
-        <a href="index.php" class="button">Index</a>
-        <a href="todo.php" class="button">Todo</a>
-    </header>
-  <div class="row">
+<div class="row">
     <div class="col-sm-2"></div>
     <div class="col-sm-8">
         <div class="card fluid">
             <h4><span class="icon-edit"></span>&nbsp;Objectif : developper un blog from scratch</h4>
             <ol>
-                <li>Découper une page html (header/content/footer)</li>
-                <li>Faire un menu de navigation entre les différentes rubriques du blog</li>
+                <li><strike>Découper une page html (header/content/footer)</strike></li>
+                <li><strike>Faire un menu de navigation entre les différentes rubriques du blog</strike></li>
                 <li>Penser le modèle Mysql qui permet de stocker les données du blog.</li>
                 <li>Développer une page de liste d'articles</li>
                 <li>Développer une page de détails d'un article (visualisation d'un article)</li>
@@ -42,8 +30,3 @@
     </div>
     <div class="col-sm-2"></div>
 </div>
-<footer class="sticky">
-  <p>LDLC - Cours Programmation Web PHP/MySql - Septembre 2020</p>
-</footer>
-</body>
-</html>
