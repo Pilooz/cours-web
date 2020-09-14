89cffcb3ebcbb7f23411e0e2ba8eb25a54b58cfe
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-09-14 21:45:42 +0200

    ok

--- workshop/content.php
+++ workshop/content.php
@@ -4,7 +4,4 @@
         Ici la liste des 6 derniers articles par lignes de 3.
     */
 
-    // require('article-mode-liste-boucle-php.php'); 
-    // require('article-mode-liste-php-my-sql.php'); 
-    require('article-mode-liste-detail-php-my-sql.php'); 
     ?>
--- workshop/todo.php
+++ workshop/todo.php
@@ -11,17 +11,18 @@
         <li class="done">Découper une page html (header/content/footer)</li>
         <li class="done">Faire un menu de navigation entre les différentes rubriques du blog</li>
         <li class="done">Penser le modèle Mysql qui permet de stocker les données du blog.</li>
-        <li class="">Développer une page de liste d'articles</li>
+        <li class="">Développer une page de liste d'articles
             <ol>
                 <li class="">Editer un nouveau fichier <b>article-mode-liste.php</b></li>
                 <li class="">L'intégrer dans content.php en utilisant 'require'.</li>
-                <li class="">dans ce fichier, écrire un modèle html qui présente les données suivantes : </li>
+                <li class="">dans ce fichier, écrire un modèle html qui présente les données suivantes :
                     <ul>
                         <li class="">les tags associés,</li>
                         <li class="">le titre de l'aricle,</li>
                         <li class="">le résumé,</li>
                         <li class="">et un bouton "lire la suite"</li>
                     </ul>
+                </li>
                 <li class="">Faire une boucle php pour génerer ces modèles d'articles</li>
                 <li class="">connecter mySQL à notre backend.</li>
                 <li class="">s'assurer que Mysql est démarré, que la base de données est bien créée (via SQLWorbench)</li>
@@ -32,17 +33,20 @@
                 <li class="">Extraire les tags rattachés aux articles et les inclure au modèle de liste d'article</li>
                 
             </ol>
-        <li class="">Développer une page de détails d'un article (visualisation d'un article)</li>
+        </li>
+        <li class="">Développer une page de détails d'un article (visualisation d'un article)
             <ol>
                 <li class="">Créer une action 'detail' et la prendre en compte de le fichier qui gère la liste des articles</li>
                 <li class="">Développer la page HTML qui présente un article en détail</li>
                 <li class="">Connecter à la BDD pour extraire les bonnes données.</li>
             </ol>
-        <li class="">Développer un formulaire d'ajout d'article</li>
+        </li>
+        <li class="">Développer un formulaire d'ajout d'article
             <ol>
                 <li class="">Créer une action 'ecrire' et la prendre en compte de le fichier qui gère la liste des articles</li>
                 <li class="">Développer la page HTML qui présente un formulaire d'ajout d'un article</li>
                 <li class="">Connecter à la BDD pour inserer les données provenant du formulaire.</li>
             </ol>
+        </li>
     </ol>
 </div>
