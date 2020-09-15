807bef4c9f24e36bd01c63ebd2ccca7f5736ac94
Pierre-Gilles Levallois <pierregille.levallois@gmail.com>
2020-09-15 16:27:35 +0200

    Avancement fin session 2

--- workshop/todo.php
+++ workshop/todo.php
@@ -11,34 +11,34 @@
         <li class="done">Découper une page html (header/content/footer)</li>
         <li class="done">Faire un menu de navigation entre les différentes rubriques du blog</li>
         <li class="done">Penser le modèle Mysql qui permet de stocker les données du blog.</li>
-        <li class="">Développer une page de liste d'articles
+        <li class="done">Développer une page de liste d'articles
             <ol>
                 <li class="done">Editer un nouveau fichier <b>article-mode-liste.php</b></li>
                 <li class="done">L'intégrer dans content.php en utilisant 'require'.</li>
-                <li class="">dans ce fichier, écrire un modèle html qui présente les données suivantes :
+                <li class="done">dans ce fichier, écrire un modèle html qui présente les données suivantes :
                     <ul>
                         <li class="done">les tags associés,</li>
                         <li class="done">le titre de l'aricle,</li>
                         <li class="done">le résumé,</li>
-                        <li class="">et un bouton "lire la suite"</li>
+                        <li class="done">et un bouton "lire la suite"</li>
                     </ul>
                 </li>
-                <li class="">Faire une boucle php pour génerer ces modèles d'articles</li>
-                <li class="">connecter mySQL à notre backend.</li>
-                <li class="">s'assurer que Mysql est démarré, que la base de données est bien créée (via SQLWorbench)</li>
-                <li class="">Saisir des articles dans la base de données (via PHPMyadmin)</li>
-                <li class="">Saisir des tags dans la table 'tag' et faire des relations entre vos tag et vos articles dans la table 'articles_tags' (PHMyAdmin)</li>
-                <li class="">Ecrire une requête SQL pour extraire les aritcles les plus récents de la BDD</li>
-                <li class="">Modifier le modèle PHP pour prendre en compte les données des articles venant de la BDD</li>
-                <li class="">Extraire les tags rattachés aux articles et les inclure au modèle de liste d'article</li>
+                <li class="done">Faire une boucle php pour génerer ces modèles d'articles</li>
+                <li class="done">connecter mySQL à notre backend.</li>
+                <li class="done">s'assurer que Mysql est démarré, que la base de données est bien créée (via SQLWorbench)</li>
+                <li class="done">Saisir des articles dans la base de données (via PHPMyadmin)</li>
+                <li class="done">Saisir des tags dans la table 'tag' et faire des relations entre vos tag et vos articles dans la table 'articles_tags' (PHMyAdmin)</li>
+                <li class="done">Ecrire une requête SQL pour extraire les aritcles les plus récents de la BDD</li>
+                <li class="done">Modifier le modèle PHP pour prendre en compte les données des articles venant de la BDD</li>
+                <li class="done">Extraire les tags rattachés aux articles et les inclure au modèle de liste d'article</li>
                 
             </ol>
         </li>
-        <li class="">Développer une page de détails d'un article (visualisation d'un article)
+        <li class="done">Développer une page de détails d'un article (visualisation d'un article)
             <ol>
-                <li class="">Créer une action 'detail' et la prendre en compte de le fichier qui gère la liste des articles</li>
-                <li class="">Développer la page HTML qui présente un article en détail</li>
-                <li class="">Connecter à la BDD pour extraire les bonnes données.</li>
+                <li class="done">Créer une action 'detail' et la prendre en compte de le fichier qui gère la liste des articles</li>
+                <li class="done">Développer la page HTML qui présente un article en détail</li>
+                <li class="done">Connecter à la BDD pour extraire les bonnes données.</li>
             </ol>
         </li>
         <li class="">Développer un formulaire d'ajout d'article
